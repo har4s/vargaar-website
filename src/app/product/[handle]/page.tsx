19c3d037cf56f6dafaa870e61553aca21e7ad5a3
har4s <khalvatihares@gmail.com>
2024-01-06 06:55:40 +0100

    -

--- src/app/product/[handle]/page.tsx
+++ src/app/product/[handle]/page.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Image from "next/image";
 import { notFound } from "next/navigation";
 import s from "./page.module.css";
 import { CoffeeTypeProduct } from "./coffee-type-product";
@@ -17,46 +16,45 @@ export default async function ProductPage({ params }: { params: { handle: string
 	return (
 		<>
 			<CoffeeTypeProduct product={product} />
-			<PageReference />
 			<RelatedProducts productId={product.id} />
 		</>
 	);
 }
 
-const PageReference: React.FC<{}> = ({}) => {
-	return (
-		<div className={cn(s.pageRef)}>
-			<div className={s.Container}>
-				<div className={s.text}>
-					<h2 className={s.title}>کاسنی ورگار را کشف کنید</h2>
-					<span className={s.subtitle}>برای مصرف بدون اعتدال!</span>
-					<div className={s.content}>
-						<p>
-							کاسنی محلول در طبیعت ارگانیک را کشف کنید. تداعی کننده طعم و مزه طبیعی خوب، انتظارات شما را با
-							نیت چشیدن یک محصول سازگار با محیط زیست با منشاء 100٪ گیاهی برآورده می کند.{" "}
-						</p>
-					</div>
-					<div className={s.actions}>
-						<Button>بیشتر بخوانید</Button>
-					</div>
-				</div>
-				<div className={s.imageContainer}>
-					<Image
-						className={s.image}
-						src="/9db5555d883bae71e59bd2e49c580d8d-1200x1200-c-default.jpg"
-						alt=""
-						width={1200}
-						height={1200}
-						data-prllxfrom='{"yPercent" : "-10"}'
-						data-prllxto='{"yPercent" : "10"}'
-						data-prllxstart="top bottom"
-						data-prllxend="bottom top"
-					/>
-				</div>
-			</div>
-		</div>
-	);
-};
+// const PageReference: React.FC<{}> = ({}) => {
+// 	return (
+// 		<div className={cn(s.pageRef)}>
+// 			<div className={s.Container}>
+// 				<div className={s.text}>
+// 					<h2 className={s.title}>کاسنی ورگار را کشف کنید</h2>
+// 					<span className={s.subtitle}>برای مصرف بدون اعتدال!</span>
+// 					<div className={s.content}>
+// 						<p>
+// 							کاسنی محلول در طبیعت ارگانیک را کشف کنید. تداعی کننده طعم و مزه طبیعی خوب، انتظارات شما را با
+// 							نیت چشیدن یک محصول سازگار با محیط زیست با منشاء 100٪ گیاهی برآورده می کند.{" "}
+// 						</p>
+// 					</div>
+// 					<div className={s.actions}>
+// 						<Button>بیشتر بخوانید</Button>
+// 					</div>
+// 				</div>
+// 				<div className={s.imageContainer}>
+// 					<Image
+// 						className={s.image}
+// 						src="/9db5555d883bae71e59bd2e49c580d8d-1200x1200-c-default.jpg"
+// 						alt=""
+// 						width={1200}
+// 						height={1200}
+// 						data-prllxfrom='{"yPercent" : "-10"}'
+// 						data-prllxto='{"yPercent" : "10"}'
+// 						data-prllxstart="top bottom"
+// 						data-prllxend="bottom top"
+// 					/>
+// 				</div>
+// 			</div>
+// 		</div>
+// 	);
+// };
 
 const RelatedProducts: React.FC<{ productId: string }> = async ({ productId }) => {
 	const products = await getProductRecommendations(productId);
